141352a1b00dd92ad24c5d681360947bf6020178
Trinity Nguyen <thn.trinity@gmail.com>
2021-03-09 17:40:19 -0800

    Finish Section 06

---
+++ section-06-programming-fundamentals/45-constants.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+const (
+	a int     = 42
+	b float64 = 42.78
+	c string  = "James Bond"
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%T\n", c)
+}
---
+++ section-06-programming-fundamentals/47-bit-shifting.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 4
+	fmt.Printf("%d\t\t%b\n", x, x)
+
+	y := x << 1
+	fmt.Printf("%d\t\t%b\n", y, y)
+
+	kb := 1024
+	mb := 1024 * kb
+	gb := 1024 * mb
+
+	fmt.Printf("%d\t\t\t%b\n", kb, kb)
+	fmt.Printf("%d\t\t\t%b\n", mb, mb)
+	fmt.Printf("%d\t\t%b\n", gb, gb)
+}
+}
